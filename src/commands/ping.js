e68f9b69e896d9c78716cce4b6d2fa5842832e73
Noxow04 <aguiral04@gmail.com>
2023-03-13 13:06:11 +0100

    slash-command ping
    from discord guide

---
+++ src/commands/ping.js
@@ -0,0 +1,11 @@
+// Basic ping command
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
+};
