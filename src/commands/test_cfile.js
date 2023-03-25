e904f0e4e8aa3367a5b328dc43e019630f82054b
FukuInTheCode <fukulovecoding@gmail.com>
2023-03-25 19:54:18 +0100

    ping command

--- src/commands/test_cfile.js
+++ src/commands/test_cfile.js
@@ -14,17 +14,17 @@ module.exports = {
         .addAttachmentOption(option => option.setName('file').setRequired(true).setDescription('my_put_nbr.c file'))
         ,
 	async execute(interaction) {
-        // URL of the image
+        // URL of the c file
 		const url = interaction.options.getAttachment('file').url;
 
 		https.get(url,(res) => {
-			// Image will be stored at this path
+			// C file downloading
 			const path = `${__dirname}/../../dumpC/my_put_nbr.c`; 
 			const filePath = fs.createWriteStream(path);
 			res.pipe(filePath);
 			filePath.on('finish',() => {
 				filePath.close();
-				interaction.reply('Download Completed'); 
+				interaction.reply('Step 1/4: Download Completed!'); 
 				// Run the .c file
 				run(path);
 			})
@@ -32,20 +32,32 @@ module.exports = {
 	},
 };
 
-const run = (path) => {
+
+// run the c file
+const run = (path, interaction) => {
 	let paths = [path];
+	// compile the c file
 	exec(`gcc -o main ${`${__dirname}/../../dumpC/main.c`} ${path}`, (err, stdout, stderr) => {
 		if (err) {
 		console.error(err);
 		return;
 		}
-
+		// move the .exe
 		fs.rename(`${__dirname}/../../main.exe`, `${__dirname}/../../dumpC/main.exe`, function (err) {
 			if (err) throw err
+
+			// test the c code
+			test(`${__dirname}/../../main.exe`, interaction);
+
 		  })
 	});
 };
 
+const test = (path) => {
+	return;
+}
+
+// delete the c file 
 const deletefile = (paths) => {
 	return;
 } 
